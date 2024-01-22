0403cb22e636d8041951401adc807d384ac43e65
mercally1 <31991854+mercally1@users.noreply.github.com>
2024-01-21 22:18:49 -0600

    Agregar archivos de proyecto.

---
+++ Operacion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Operacion
+    {
+        private double resultado;
+        private double parametro1, parametro2;
+
+        public double Resultado { get => resultado; set => resultado = value; }
+        public double Parametro1 { get => parametro1; set => parametro1 = value; }
+        public double Parametro2 { get => parametro2; set => parametro2 = value; }
+
+        public double multiplicacion (double valor1, double valor2)
+        {
+            resultado = valor1 * valor2;
+            return resultado;
+        }
+
+        public double multiplicacion()
+        {
+            this.Resultado = this.Parametro1 * Parametro2;
+            return this.Resultado;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Operacion objop = new Operacion();
+            //Console.WriteLine($"5.2 * 9.1 = {objop.multiplicacion(5.2, 9.1)}");
+
+            Console.Write("Introduce un numero");
+            objop.Parametro1= Double.Parse(Console.ReadLine());
+
+            Console.Write("Introduce un numero");
+            objop.Parametro2 = Double.Parse(Console.ReadLine());
+
+            Console.Write("El resultado es: " +  objop.multiplicacion());
+
+            Console.ReadLine();
+        }
+    }
+}
